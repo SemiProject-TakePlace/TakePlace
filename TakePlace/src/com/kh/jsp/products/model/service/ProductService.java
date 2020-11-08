b36ccfe1ab6bf0f33bc203024b42e069abcde17b
JongYeon Kim <jongyeon.kim@daum.net>
2020-11-08 14:07:35 +0900

    2020-11-08 14:02 Product List Servlet (go to productDetail)

--- TakePlace/src/com/kh/jsp/products/model/service/ProductService.java
+++ TakePlace/src/com/kh/jsp/products/model/service/ProductService.java
@@ -4,6 +4,7 @@ import static com.kh.jsp.common.JDBCTemplate.*;
 
 import java.sql.Connection;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import com.kh.jsp.products.model.dao.ProductDAO;
 import com.kh.jsp.products.model.vo.Product;
@@ -94,6 +95,16 @@ public class ProductService {
 		close(con);
 		
 		return result;
+	}
+
+	public HashMap<String, Object> selectOne(int pno) {
+		con = getConnection();
+		
+		HashMap<String, Object> hmap = pDAO.selectOne(con, pno);
+		
+		close(con);
+		
+		return hmap;
 	}
 
 
