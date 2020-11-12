a1683d979989285f8b0c4202ecd36d821450ec2a
je <je@DESKTOP-UVQV38R>
2020-11-12 18:52:21 +0900

    2020-11-12 18:51 pay 수정 & 예약 요청 페이지 수정(완료 x) 하지만 실행은 o - kde

--- TakePlace/src/com/kh/jsp/payrecord/model/dao/PayDAO.java
+++ TakePlace/src/com/kh/jsp/payrecord/model/dao/PayDAO.java
@@ -11,9 +11,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Properties;
 
-import com.kh.jsp.common.exception.NoticeException;
-import com.kh.jsp.payrecord.model.vo.PayRecord;
-import com.kh.jsp.products.model.vo.Product;
 import com.kh.jsp.reservation.model.dao.ReservationDAO;
 import com.kh.jsp.reservation.model.vo.Reservation;
 
@@ -37,7 +34,7 @@ public class PayDAO {
 		}
 	}
 
-	public Reservation selectOnePay(Connection con, int preqno) {
+	public Reservation selectOnePay(Connection con,int pno, int preqno) {
 		Reservation r = null;
 		PreparedStatement pstmt = null;
 		ResultSet rset = null;
@@ -48,6 +45,7 @@ public class PayDAO {
 			pstmt = con.prepareStatement(sql);
 			
 			pstmt.setInt(1, preqno);
+			//pstmt.setInt(1, pno);
 			
 			rset = pstmt.executeQuery();
 			
@@ -65,7 +63,7 @@ public class PayDAO {
 			close(pstmt);
 		}
 		
-		System.out.println(r);
+		//System.out.println(r);
 		
 		return r;
 	}
@@ -83,9 +81,9 @@ public class PayDAO {
 			
 			result = pstmt.executeUpdate();
 			
-			System.out.println(mno);
-			System.out.println(preqno);
-			System.out.println("result : " + result);
+//			System.out.println(mno);
+//			System.out.println(preqno);
+//			System.out.println("result : " + result);
 			
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -96,7 +94,7 @@ public class PayDAO {
 		return result;
 	}
 
-	public int updatePayCount(Connection con, int pno) {
+	public int updatePayCount(Connection con, int pno, int preqno) {
 		int result = 0;
 		PreparedStatement pstmt = null;
 		
@@ -105,7 +103,8 @@ public class PayDAO {
 		try {
 			pstmt = con.prepareStatement(sql);
 			
-			pstmt.setInt(1, pno);
+			pstmt.setInt(1, preqno);
+			pstmt.setInt(2, pno);
 			
 			result = pstmt.executeUpdate();
 			
